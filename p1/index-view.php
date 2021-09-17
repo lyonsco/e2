baa39582ba5b659b827bd29a0f44f0f0b3716e91
Courtney Lyons <lyonsco@gmail.com>
2021-09-17 17:26:27 +0000

    Added mechanics to p1/index-view.php

--- p1/index-view.php
+++ p1/index-view.php
@@ -8,11 +8,19 @@
 </head>
 
 <body>
-    <h1>Project 1</h1>
+    <h1>Number Matching Game</h1>
 
     <h2>Mechanics</h2>
     <ul>
-        <li>TBD...</li>
+        <li>A random number between 1 and 10 is generated for Player 1.</li>
+        <li>Player 2 needs to match that number in as few tries as possible.</li>
+        <li>A random number between 1 and 10 is generated for Player 2.</li>
+        <li>If Player 1's number matches Player 2's number, they get 1 point. If not, another random number is generated
+            for Player 2.</li>
+        <li>If this second number matches Payer 1's, Player 2 gets 2 points. And so on until there is a match./li>
+        <li>Once Player 2 matches Player 1's number, it is Player 2's turn to start with a random number for Player 1 to
+            match.</li>
+        <li>Play as many rounds as you would like. The player with the lowest point total wins./li>
     </ul>
 
     <h2>Results</h2>
