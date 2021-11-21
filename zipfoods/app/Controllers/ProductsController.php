1d764f8115e87bfa087a4eb04e13126a4dd7ac95
Courtney Lyons <lyonsco@gmail.com>
2021-11-21 17:55:03 +0000

    Week 12 work

--- zipfoods/app/Controllers/ProductsController.php
+++ zipfoods/app/Controllers/ProductsController.php
@@ -27,6 +27,9 @@ class ProductsController extends Controller
     {
         $sku = $this->app->param('sku');
 
+        if(is_null($sku)) {
+            $this->app->redirect('/products');
+        }
         $productsObj = new Products($this->app->path('/database/products.json'));
 
         $product = $productsObj->getBySku($sku);
@@ -35,6 +38,66 @@ class ProductsController extends Controller
             return $this->app->view('products/missing');
         }
         
-        return $this->app->view('products/show', ['product' => $product]);
+        $reviewSaved = $this->app->old('reviewSaved');
+
+        return $this->app->view('products/show', [
+            'product' => $product,
+            'reviewSaved' => $reviewSaved
+        ]);
+    }
+
+    public function saveReview()
+    {
+        $this->app->validate([
+            'sku' => 'required',
+            'name' => 'required',
+            'review' => 'required|minLength:200'
+        ]);
+
+        # If the above validation checks fail
+        # The user is redirected back to where they came from (/product)
+        # None of the code that follows will be executed
+        
+        $sku = $this->app->input('sku');
+        $name = $this->app->input('name');
+        $review = $this->app->input('review');
+
+        #To Do - Persist review to the database
+        $host = $this->app->env('DB_HOST');
+        $database = $this->app->env('DB_NAME');
+        $username = $this->app->env('DB_USERNAME');
+        $password = $this->app->env('DB_PASSWORD');
+        
+        # DSN (Data Source Name) string
+        # contains the information required to connect to the database
+        $dsn = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+    
+        # Driver-specific connection options
+        $options = [
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+            \PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+    
+        try {
+            # Create a PDO instance representing a connection to a database
+            $pdo = new \PDO($dsn, $username, $password, $options);
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    
+        $sqlTemplate = "INSERT INTO reviews (sku, name, review) 
+VALUES (:sku, :name, :review)";
+
+$values = [
+    'sku' => $this->app->input('sku'),
+    'name' => $this->app->input('name'),
+    'review' => $this->app->input('review'),
+];
+
+$statement = $pdo->prepare($sqlTemplate);
+$statement->execute($values);
+
+        return $this->app->redirect('/product?sku=' . $sku, ['reviewSaved' => true]);
     }
 }
